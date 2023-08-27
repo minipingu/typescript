950b4b357cc55752600de0acb0d475918b73a17f
Dede Nurhidayat <ranmaniax@gmail.com>
2023-08-28 06:25:29 +0700

    4/3.Union Type

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,27 @@
-"use strict";
-let numbers = [];
-var Size;
-(function (Size) {
-    Size[Size["Small"] = 1] = "Small";
-    Size[Size["Medium"] = 2] = "Medium";
-    Size[Size["Large"] = 3] = "Large";
-})(Size || (Size = {}));
-console.log(Size.Large);
+'use strict'
+function calculateTax(income, taxYear = 2022) {
+	if (taxYear < 50000) {
+		return income * 1.2
+	}
+	return income * 1.3
+}
+calculateTax(1)
+let employee = {
+	id: 1,
+	name: 'Minipingu',
+	retire: (date) => {
+		console.log(date)
+	},
+}
+employee.name = 'Mosh'
+
+function kgToLbs(weight) {
+	if (typeof weight === 'number') {
+		return weight * 2.2
+	} else {
+		return parseInt(weight) * 2.2
+	}
+}
+kgToLbs(10)
+kgToLbs('10kg')
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,49 @@
-let numbers:number[] = []
-enum Size {
-   Small = 1,
-   Medium = 2,
-   Large = 3
+// enum
+// let numbers:number[] = []
+// enum Size {
+//    Small = 1,
+//    Medium = 2,
+//    Large = 3
+// }
+
+// console.log(Size.Large)
+
+//function
+function calculateTax(income: number, taxYear = 2022) {
+	if (taxYear < 50000) {
+		return income * 1.2
+	}
+	return income * 1.3
+}
+calculateTax(1)
+//
+
+//2. type alias
+type Employee = {
+	readonly id: number
+	name: string
+	retire: (date: Date) => void
 }
 
-console.log(Size.Large)
+let employee: Employee = {
+	id: 1,
+	name: 'Minipingu',
+	retire: (date: Date) => {
+		console.log(date)
+	},
+}
+employee.name = 'Mosh'
+
+//
+//3. Union type
+function kgToLbs(weight: number | string): number {
+	//Narrowing
+	if (typeof weight === 'number') {
+		return weight * 2.2
+	} else {
+		return parseInt(weight) * 2.2
+	}
+}
+
+kgToLbs(10)
+kgToLbs('10kg')
